8f94e46052c8873a6461437859326313f5f9453d
Luis Angel Contreras Cerón <redfoxycc@gmail.com>
2026-05-06 16:33:40 -0600

    A3

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
-    window.onload = function() {
+window.onload = function() {
+    if (document.querySelector('.swiper-hero')) {
         const swiper = new Swiper('.swiper-hero', {
             loop: true,
             observer: true,
@@ -16,29 +17,24 @@
                 prevEl: '.swiper-button-prev',
             },
         });
-    };
+    }
 
-    const swiperMarcas = new Swiper('.swiper-marcas', {
-        slidesPerView: 2,      // Cuántos logos se ven a la vez
-        spaceBetween: 30,
-        loop: true,
-        freeMode: false, 
-        speed: 8000,           // 8 secs
-        autoplay: {
-        delay: 0,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: true,
-        allowTouchMove: true,
-        enabled: true,
-        sticky: true,
-        momentum: false,
-        },
-        preventClicks: true,
-        preventClicksPropagation: true,
-    breakpoints: {
-        // Ajuste para celulares
-    320: { slidesPerView: 2, spaceBetween: 50},
-    768: { slidesPerView: 4, spaceBetween: 50},
-    1024: { slidesPerView: 6, spaceBetween: 50}
-        }
-    });
+    if (document.querySelector('.swiper-marcas')) {
+        const swiperMarcas = new Swiper('.swiper-marcas', {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            loop: true,
+            speed: 8000,
+            autoplay: {
+                delay: 0,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+            },
+            breakpoints: {
+                320: { slidesPerView: 2, spaceBetween: 50},
+                768: { slidesPerView: 4, spaceBetween: 50},
+                1024: { slidesPerView: 6, spaceBetween: 50}
+            }
+        });
+    }
+};
